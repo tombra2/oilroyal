f43ee95481ad9f457d4d29c9be1218db3034d993
Brandner <brandner@akaryon.com>
2025-10-29 19:49:02 +0100

    landigpage

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,7 @@
-import './bootstrap.js';
+import './bootstrap.js'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css';
 /*
  * Welcome to your app's main JavaScript file!
  *
